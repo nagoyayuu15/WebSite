55ceb9459b51ed75975455138c394a12fbb5e034
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-04-14 00:41:08 +0900

    update weblog

--- portfolio/js/define_updated_time.js
+++ portfolio/js/define_updated_time.js
@@ -1,2 +1,2 @@
-const url_param_updated_at = '?updatedAt="Sun, 07 Apr 2024 23:03:01 GMT"&'
+const url_param_updated_at = '?updatedAt="Wed, 10 Apr 2024 14:17:23 GMT"&'
 const url_param_fragment_version = '?fragment_version=4.2&'
