ad6fff1e1f572fd1db24dad85b44f1795b4d30a1
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-05-10 09:33:48 +0900

    article update

--- portfolio/js/define_updated_time.js
+++ portfolio/js/define_updated_time.js
@@ -1,2 +1,2 @@
-const url_param_updated_at = '?updatedAt="Fri, 26 Apr 2024 04:40:14 GMT"&'
+const url_param_updated_at = '?updatedAt="Fri, 03 May 2024 09:43:25 GMT"&'
 const url_param_fragment_version = '?fragment_version=4.2&'
