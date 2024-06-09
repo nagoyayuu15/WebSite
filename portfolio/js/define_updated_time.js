64851fc86b6cc6d49319c27d253374ed73bc278e
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-06-09 18:57:59 +0900

    20240609の更新

--- portfolio/js/define_updated_time.js
+++ portfolio/js/define_updated_time.js
@@ -1,2 +1,2 @@
-const url_param_updated_at = '?updatedAt="Mon, 20 May 2024 14:16:42 GMT"&'
+const url_param_updated_at = '?updatedAt="Sun, 09 Jun 2024 09:57:42 GMT"&'
 const url_param_fragment_version = '?fragment_version=4.2&'
